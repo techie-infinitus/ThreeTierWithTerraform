e814335e774f2396623c836b385c9c750871b744
techie-infinitus <87640980+techie-infinitus@users.noreply.github.com>
2022-11-17 20:36:18 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  access_key = "AKIASMIXNH4FEH6N4E25"
-  secret_key = "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"
+  access_key = "xxx"
+  secret_key = "xxx"
   region = "eu-west-2"
 }
